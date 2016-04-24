b6cc39bc4a10a8543f1ebbf4221b3cf2df4b79d5
tanyaweaver <tanya83@uw.edu>
2016-04-24 15:28:31 -0700

    done

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-var checkbox = document.getElementById('form');
+var input1 = document.getElementsByTagName('one');
+var label = document.getElementsByTagName('label');
+function handleCheckbox () {
+  if (label.classList.contains('line-through')){
+    label.classList.remove('line-through');
+  }else{
+    label.classList.add('line-through');
+  }
+}
+input1.addEventListener('click', handleCheckbox);
